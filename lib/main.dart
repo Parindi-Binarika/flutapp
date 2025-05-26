0439c34b7e473d8603e759cb3c6f706a676fa9e8
Parindi2005 <pbinarika@gmail.com>
2025-05-26 13:59:08 +0400

    test commit2

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("IOT APP 08"),
+          title: Text("IOT  new APP 08"),
           backgroundColor: const Color.fromARGB(255, 132, 195, 165),
           centerTitle: true,
         ),
